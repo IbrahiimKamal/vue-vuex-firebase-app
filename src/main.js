fc562e45051ff7d0ed01b0d737d53fed6bbd2229
IbrahiimKamal <ebrahiiim.kamal@gmail.com>
2022-06-10 00:05:09 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 library.add(faStar);
 
 const app = createApp(App);
 
-app.use(router);
+app.use(store).use(router);
+
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
