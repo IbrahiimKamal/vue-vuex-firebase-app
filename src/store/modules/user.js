9afe5e59c46eff22f24bb6f02034777bfc96e62b
IbrahiimKamal <ebrahiiim.kamal@gmail.com>
2022-06-11 12:46:12 +0200

    add login and register pages and create a user account on firebase

---
+++ src/store/modules/user.js
@@ -0,0 +1,45 @@
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+export default {
+  namespaced: true,
+
+  state() {
+    return {
+      register: {
+        isProcessing: false,
+        error: '',
+      },
+    };
+  },
+
+  mutations: {
+    setRegisterIsProcessing(state, isProcessing) {
+      state.register.isProcessing = isProcessing;
+    },
+
+    setRegisterError(state, error) {
+      state.register.error = error;
+    },
+  },
+
+  actions: {
+    async register({ commit, dispatch }, { email, password }) {
+      commit('setRegisterIsProcessing', true);
+      commit('setRegisterError', '');
+
+      try {
+        const userCredentials = await createUserWithEmailAndPassword(
+          getAuth(),
+          email,
+          password
+        );
+        return userCredentials.user;
+      } catch (e) {
+        commit('setRegisterError', e.message);
+        dispatch('toast/error', e.message, { root: true });
+      } finally {
+        commit('setRegisterIsProcessing', false);
+      }
+    },
+  },
+};
